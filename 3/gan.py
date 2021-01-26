d4af3208bfa5c76cc2fec4ff7e3a710b31f7c1c2
itzik1058 <itzik1058@gmail.com>
2021-01-26 21:40:32 +0200

    DCGAN
    Co-Authored-By: Ben Filiarsky <74211354+benpili@users.noreply.github.com>

--- 3/gan.py
+++ 3/gan.py
@@ -1,22 +1,15 @@
+import torch
 import torch.nn as nn
 
 
-def weights_init(w):
-    classname = w.__class__.__name__
-    if classname.find('conv') != -1:
-        nn.init.normal_(w.weight.data, 0.0, 0.02)
-    elif classname.find('bn') != -1:
-        nn.init.normal_(w.weight.data, 1.0, 0.02)
-        nn.init.constant_(w.bias.data, 0)
-
-
 class Generator(nn.Module):
-    def __init__(self, latent_size, channels, hidden_dim):
+    def __init__(self, channels, latent_size, num_classes, hidden_dim):
         super(Generator, self).__init__()
         self.latent_size = latent_size
-        self.hidden_dim = hidden_dim
+        self.num_classes = num_classes
+        combined_size = latent_size + num_classes
         self.generator = nn.Sequential(
-            nn.ConvTranspose2d(latent_size, hidden_dim * 8, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.ConvTranspose2d(combined_size, hidden_dim * 8, kernel_size=4, stride=1, padding=0, bias=False),
             nn.BatchNorm2d(hidden_dim * 8),
             nn.ReLU(inplace=True),
             nn.ConvTranspose2d(hidden_dim * 8, hidden_dim * 4, kernel_size=4, stride=2, padding=1, bias=False),
@@ -32,8 +25,10 @@ class Generator(nn.Module):
             nn.Tanh()
         )
 
-    def forward(self, latent):
-        return self.generator(latent.view(-1, self.latent_size, 1, 1))
+    def forward(self, latent, attr):
+        cat = torch.cat((latent, attr), dim=1)
+        x = self.generator(cat.view(-1, self.latent_size + self.num_classes, 1, 1))
+        return x
 
 
 class Discriminator(nn.Module):
@@ -42,24 +37,32 @@ class Discriminator(nn.Module):
         self.discriminator = nn.Sequential(
             nn.Conv2d(channels, hidden_dim, kernel_size=4, stride=2, padding=1, bias=False),
             nn.LeakyReLU(negative_slope=0.2, inplace=True),
+            nn.Dropout2d(0.25),
             nn.Conv2d(hidden_dim, hidden_dim * 2, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(hidden_dim * 2),
+            nn.BatchNorm2d(hidden_dim * 2, 0.8),
             nn.LeakyReLU(negative_slope=0.2, inplace=True),
+            nn.Dropout2d(0.25),
             nn.Conv2d(hidden_dim * 2, hidden_dim * 4, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(hidden_dim * 4),
+            nn.BatchNorm2d(hidden_dim * 4, 0.8),
             nn.LeakyReLU(negative_slope=0.2, inplace=True),
+            nn.Dropout2d(0.25),
             nn.Conv2d(hidden_dim * 4, hidden_dim * 8, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(hidden_dim * 8),
+            nn.BatchNorm2d(hidden_dim * 8, 0.8),
             nn.LeakyReLU(negative_slope=0.2, inplace=True),
+            nn.Dropout2d(0.25),
             nn.Conv2d(hidden_dim * 8, 1, kernel_size=4, stride=1, padding=0, bias=False),
             nn.Sigmoid()
         )
 
     def forward(self, image):
-        return self.discriminator(image).view(-1)
+        result = self.discriminator(image)
+        return result.view(-1)
 
 
-def make_model(channels, latent_size, hidden_dim):
-    generator = Generator(latent_size, channels, hidden_dim)
-    discriminator = Discriminator(channels, hidden_dim)
-    return generator, discriminator
+def weights_init_normal(m):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        torch.nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find("BatchNorm2d") != -1:
+        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+        torch.nn.init.constant_(m.bias.data, 0.0)
--- 3/train.py
+++ 3/train.py
@@ -6,36 +6,50 @@ import torchvision.datasets as datasets
 import gan
 import time
 import matplotlib.pyplot as plt
+import imageio
+import skimage
+import glob
 
 
-def train(generator, discriminator, train_loader, n_epoch, **kwargs):
-    generator.cuda()
-    discriminator.cuda()
+def train():
+    n_epoch, lr, betas = 50, 0.0002, (0.5, 0.999)
+    transform = transforms.Compose([transforms.Resize(64), transforms.CenterCrop(64),
+                                    transforms.RandomHorizontalFlip(), transforms.ToTensor(),
+                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    train_dataset = datasets.CelebA('', split='train', transform=transform, download=True)
+    train_loader = data.DataLoader(train_dataset, batch_size=128, shuffle=True)
+    generator = gan.Generator(channels=3, latent_size=128, num_classes=2, hidden_dim=64).cuda()
+    discriminator = gan.Discriminator(channels=3, hidden_dim=64).cuda()
+    generator.apply(gan.weights_init_normal)
+    discriminator.apply(gan.weights_init_normal)
     generator.train()
     discriminator.train()
-    gen_optimizer = torch.optim.Adam(generator.parameters(), **kwargs)
-    disc_optimizer = torch.optim.Adam(discriminator.parameters(), **kwargs)
+    gen_optimizer = torch.optim.Adam(generator.parameters(), lr=lr, betas=betas)
+    disc_optimizer = torch.optim.Adam(discriminator.parameters(), lr=lr, betas=betas)
     criterion = torch.nn.BCELoss()
     generator_train_loss, discriminator_train_loss = [], []
     example = torch.randn(64, generator.latent_size).cuda()
+    example_attr = torch.randint(2, size=(64, 2)).float().cuda()
     try:
         for epoch in range(n_epoch):
             epoch_time = time.time()
             generator_train_loss.append(0)
             discriminator_train_loss.append(0)
-            for image, _ in train_loader:
+            for image, attr in train_loader:
                 batch_size = image.size(0)
                 image = image.cuda()
+                attr = attr[:, [15, 20]].float().cuda()
                 real_label = torch.ones(batch_size).cuda()
                 fake_label = torch.zeros(batch_size).cuda()
 
+                latent = torch.randn(batch_size, generator.latent_size).cuda()
+                generated = generator(latent, attr)
+
                 disc_optimizer.zero_grad()
                 real_loss = criterion(discriminator(image), real_label)
-                real_loss.backward()
-                latent = torch.randn(batch_size, generator.latent_size).cuda()
-                generated = generator(latent)
                 fake_loss = criterion(discriminator(generated.detach()), fake_label)
-                fake_loss.backward()
+                disc_loss = real_loss + fake_loss
+                disc_loss.backward()
                 disc_optimizer.step()
 
                 gen_optimizer.zero_grad()
@@ -44,37 +58,67 @@ def train(generator, discriminator, train_loader, n_epoch, **kwargs):
                 gen_optimizer.step()
 
                 generator_train_loss[-1] += gen_loss.item()
-                discriminator_train_loss[-1] += real_loss.item() + fake_loss.item()
+                discriminator_train_loss[-1] += disc_loss.item()
             generator_train_loss[-1] /= train_loader.batch_size
             discriminator_train_loss[-1] /= train_loader.batch_size
             if epoch % 1 == 0:
                 with torch.no_grad():
-                    generated = generator(example).detach().cpu()
+                    generated = generator(example, example_attr).detach().cpu()
                 grid = torchvision.utils.make_grid(generated, padding=2, normalize=True)
                 plt.imshow(grid.numpy().transpose(1, 2, 0))
+                plt.axis('off')
+                plt.grid()
+                plt.savefig(f'train_img/{epoch}.png')
                 plt.show()
             print(f'Epoch {epoch}/{n_epoch} done in {time.time() - epoch_time:.2f}s with loss '
                   f'Generator({generator_train_loss[-1]:.3f}) Discriminator({discriminator_train_loss[-1]:.3f})')
+            torch.save(generator.state_dict(), f'model/generator_{epoch}.pkl')
     except KeyboardInterrupt:
         pass
-    torch.save(generator.state_dict(), 'generator.pkl')
+    torch.save(generator.state_dict(), f'generator.pkl')
     plt.plot(generator_train_loss, label='Generator')
     plt.plot(discriminator_train_loss, label='Discriminator')
-    plt.xticks(range(n_epoch))
     plt.xlabel('Epoch')
     plt.title('BCE Loss')
     plt.legend()
+    plt.savefig('loss.png')
     plt.show()
 
 
-def main():
-    transform = transforms.Compose([transforms.Resize(64), transforms.CenterCrop(64), transforms.ToTensor(),
-                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    train_dataset = datasets.CelebA('', split='train', transform=transform, download=True)
-    train_loader = data.DataLoader(train_dataset, batch_size=128, shuffle=True)
-    generator, discriminator = gan.make_model(channels=3, latent_size=100, hidden_dim=64)
-    train(generator, discriminator, train_loader, n_epoch=10, lr=0.0002, betas=(0.5, 0.999))
+def interpolate(path, generator, latent, attr, steps=60):
+    images = []
+    for k in range(len(latent) - 1):
+        for i in range(steps + 1):
+            t = i / steps
+            lat = (1 - t) * latent[k] + t * latent[k + 1]
+            a = (1 - t) * attr[k] + t * attr[k + 1]
+            with torch.no_grad():
+                generated = generator(lat, a).detach().cpu()
+            images.append(torchvision.utils.make_grid(generated, padding=2, normalize=True).numpy().transpose(1, 2, 0))
+    images = [skimage.img_as_ubyte(img) for img in images]
+    imageio.mimsave(path, images + images[::-1], fps=30)
+
+
+def make_results():
+    generator = gan.Generator(channels=3, latent_size=128, num_classes=2, hidden_dim=64).cuda()
+    generator.load_state_dict(torch.load('generator.pkl'))
+    generator.eval()
+    files = sorted(glob.glob('train_img/*.png'), key=lambda n: int(n.split('\\')[-1].replace('.png', '')))
+    images = [skimage.img_as_ubyte(plt.imread(name)) for name in files]
+    imageio.mimsave('result/train.gif', images, fps=5)
+    size = 64
+    latent = []
+    attr = []
+    for _ in range(3):
+        latent.append(torch.randn(size, generator.latent_size).cuda())
+        attr.append(torch.randint(2, size=(size, 2)).float().cuda())
+    interpolate('result/interpolation.gif', generator, latent, attr)
+
+
+def reproduce_hw3():
+    train()
+    make_results()
 
 
 if __name__ == '__main__':
-    main()
+    reproduce_hw3()
